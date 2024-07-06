e51a22a45ce8962972337c49ba5b6924a7c8a22e
rhdedgar23 <96807850+rhdedgar23@users.noreply.github.com>
2024-07-05 22:45:20 -0600

    Encapsulamiento. Nombre de paquetes. Getters y setters. Palabra clave "this".

---
+++ Screenmatch/src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -0,0 +1,62 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Pelicula {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnPlan;
+    private double sumaDeLasEvaluaciones;
+    private int totalDeLasEvaluaciones;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnPlan(boolean incluidoEnPlan) {
+        this.incluidoEnPlan = incluidoEnPlan;
+    }
+
+    public int getTotalDeLasEvaluaciones(){
+        return totalDeLasEvaluaciones;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnPlan() {
+        return incluidoEnPlan;
+    }
+
+    public void muestraFichaTecnica(){
+        System.out.println("El nombre de la pelicula es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Duracion en minutos: " + duracionEnMinutos);
+    }
+
+    public void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeLasEvaluaciones ++;
+
+    }
+
+    public double calculaMedia(){
+        return sumaDeLasEvaluaciones/totalDeLasEvaluaciones;
+    }
+}
